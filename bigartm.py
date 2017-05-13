b0603295588f64f61a2d2872956f2c8c90e7064f
SmirnovEgorRu <smirnov.egor.ru@gmail.com>
2017-05-13 06:20:37 +0300

    1

--- bigartm.py
+++
@@ -1,4 +0,0 @@
-from matplotlib import pyplot as plt
-import artm
-
-batch_vectorizer=artm.BatchVectorizer(data_path="var.txt",data_format="vompal_wabbit",target_folder="var_batches",batch_size=100)
